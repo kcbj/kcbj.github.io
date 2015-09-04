f766331337619e11a76e37164d9f04c44caaa7c0
Rutger Claes <rgc@rgc.be>
2015-09-04 12:13:59 +0200

    Location address

--- _locations/generate.rb
+++ _locations/generate.rb
@@ -3,6 +3,6 @@ require 'yaml'
 
 locations = YAML::load_file( '../_data/locations.yml' )
 locations.each_with_index do |location,index|
-  data = location.merge( 'order' => index, 'title' => data[ 'name' ] )
-  File.open( "#{data['id']}.html", 'w' ) << data.to_yaml + "\n---"
+  data = location.merge( 'order' => index, 'title' => location[ 'name' ] )
+  File.open( "#{data['id']}.html", 'w' ) << data.to_yaml + "---"
 end
